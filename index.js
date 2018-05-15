a61baa8417b6e1e65efc4ed7f4b5f9ad83562aa8
Bill Parrott <bill@chimericdream.com>
2018-05-14 23:22:41 -0500

    almost ready for 2.0

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-/* global module */
+/* global module, require */
 
-const possibleErrors = require('./rules/core/possible-errors.js');
-const bestPractices = require('./rules/core/best-practices.js');
-const variables = require('./rules/core/variables.js');
-const nodejsCommonjs = require('./rules/core/nodejs-commonjs.js');
-const stylisticIssues = require('./rules/core/stylistic-issues.js');
-const es6 = require('./rules/core/es6.js');
+const possibleErrors = require('./lib/rules/core/possible-errors.js');
+const bestPractices = require('./lib/rules/core/best-practices.js');
+const variables = require('./lib/rules/core/variables.js');
+const nodejsCommonjs = require('./lib/rules/core/nodejs-commonjs.js');
+const stylisticIssues = require('./lib/rules/core/stylistic-issues.js');
+const es6 = require('./lib/rules/core/es6.js');
 
-const babel = require('./rules/plugins/babel.js');
+const babel = require('./lib/rules/plugins/babel.js');
+const filenames = require('./lib/rules/plugins/filenames.js');
+const sortClassMembers = require('./lib/rules/plugins/sort-class-members.js');
 
 module.exports = {
-    'env': {
-        'es6': true,
-        'node': true,
+    env: {
+        es6: true,
+        node: true,
     },
-    'parser': 'babel-eslint',
-    'parserOptions': {
-        'ecmaFeatures': {
-            'experimentalObjectRestSpread': true,
-            'jsx': true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true,
         },
-        'ecmaVersion': 6,
-        'sourceType': 'module',
+        ecmaVersion: 6,
+        sourceType: 'module',
     },
-    'plugins': [
-        'babel',
-        'import'
-    ],
-    'rules': {
-        'strict': ['error', 'global'],
+    plugins: ['babel', 'filenames', 'sort-class-members'],
+    rules: {
+        strict: ['error', 'global'],
         ...possibleErrors,
         ...bestPractices,
         ...variables,
@@ -36,5 +35,7 @@ module.exports = {
         ...stylisticIssues,
         ...es6,
         ...babel,
+        ...filenames,
+        ...sortClassMembers,
     },
 };
