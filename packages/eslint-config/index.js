5550f6e8f675d96d284b297bf04e416de6237816
Bill Parrott <bill@chimericdream.com>
2020-09-22 22:12:06 -0500

    fix bad import

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,7 +3,6 @@
 const possibleErrors = require(`${__dirname}/lib/rules/core/possible-errors.js`);
 const bestPractices = require(`${__dirname}/lib/rules/core/best-practices.js`);
 const variables = require(`${__dirname}/lib/rules/core/variables.js`);
-const nodejsCommonjs = require(`${__dirname}/lib/rules/core/nodejs-commonjs.js`);
 const stylisticIssues = require(`${__dirname}/lib/rules/core/stylistic-issues.js`);
 const es6 = require(`${__dirname}/lib/rules/core/es6.js`);
 
@@ -13,7 +12,6 @@ module.exports = {
         ...possibleErrors,
         ...bestPractices,
         ...variables,
-        ...nodejsCommonjs,
         ...stylisticIssues,
         ...es6,
     },
